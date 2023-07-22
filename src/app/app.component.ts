ea459b36777c5b6626190a00326ccfee7c459cce
Bartek Kawecki <bkawecki21@gmail.com>
2023-07-22 16:08:40 +0200

    finished up task logic, added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task('Visit Ann'),
+    new Task('Call dad'),
+    new Task('Go to the gym'),
+    new Task('Wash the dishes'),
+    new Task('Buy groceries')
+  ]
+
+  add(task: string) {
+    this.tasks.push(new Task(task));
+  }
+
+  remove(existingTask: Task) {
+    let userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask.title}"`);
+    
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
+}
+
+class Task {
+
+  constructor(public title: string) {
+
+  }
+
+  toggleIsDone() {
+    this.isDone = !this.isDone;
+  }
+
+  public isDone = false;
 }
